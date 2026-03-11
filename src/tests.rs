7724c961db2166adb11c9867d50d5d1f8db01cbf
Félix Saparelli <felix@bes.au>
2026-03-11 22:50:07 +1300

    test: verify app global spec items
    verify: app.var, app.type, app.constructor, app.methods, app.resources, app.resources.static, app.resources.dynamic, app.resources.names, param.type

--- src/tests.rs
+++ src/tests.rs
@@ -24,3 +24,4 @@ pub fn exercise(source: &str) {
 }
 
 mod bsl;
+mod app;
---
+++ src/tests/app.rs
@@ -0,0 +1,117 @@
+use super::*;
+use defs::resource::ResourceKind;
+
+// l[verify app.var]
+// l[verify app.type]
+// l[verify app.constructor]
+#[test]
+fn app_global_is_available_and_not_constructible() {
+    run_test_script_app(
+        r#"
+        let t = app.type_of();
+        if t != "App" { throw "app must be of type App, got: " + t; }
+    "#,
+    );
+}
+
+// l[verify app.methods]
+#[test]
+fn app_methods_are_defined() {
+    run_test_script_app(
+        r#"
+        app.service("svc");
+        app.deployment("dep");
+        app.job("jb");
+        app.volume("vol");
+        app.external_volume("evol");
+        app.external_service("esvc");
+        app.param("p");
+        app.on_action("act", |rt| {});
+        app.on_start(|rt| {});
+        app.on_upgrade(|rt, old| {});
+        app.on_crash_recovery(|rt, history| {});
+        app.on_shell("sh", |rt| { app.job("sh").command("sh") });
+        app.on_install(|rt, reqs| {});
+    "#,
+    );
+}
+
+// l[verify app.resources]
+#[test]
+fn app_holds_resources() {
+    let app = run_test_script_app(r#"let s = app.service("web");"#);
+    let def = app.0.lock();
+    assert!(
+        def.resources
+            .keys()
+            .any(|id| id.kind == ResourceKind::Service && &*id.name == "web")
+    );
+}
+
+// l[verify app.resources.static]
+#[test]
+fn top_level_resources_are_static() {
+    let app = run_test_script_app(
+        r#"
+        let svc = app.service("static-svc");
+        let dep = app.deployment("static-dep");
+    "#,
+    );
+    let def = app.0.lock();
+    assert!(
+        def.resources
+            .keys()
+            .any(|id| id.kind == ResourceKind::Service && &*id.name == "static-svc")
+    );
+    assert!(
+        def.resources
+            .keys()
+            .any(|id| id.kind == ResourceKind::Deployment && &*id.name == "static-dep")
+    );
+}
+
+// l[verify app.resources.dynamic]
+#[test]
+fn closures_create_dynamic_resources() {
+    let app = run_test_script_app(
+        r#"
+        let make_job = || app.job("ephemeral")
+            .image("tools:1")
+            .command("run");
+
+        app.on_start(|rt| {
+            let j = make_job.call();
+            rt.start(j).terminated();
+        });
+    "#,
+    );
+    let def = app.0.lock();
+    assert!(def.actions.contains_key("start"));
+}
+
+// l[verify app.resources.names]
+#[test]
+fn same_name_returns_same_resource() {
+    let app = run_test_script_app(
+        r#"
+        let a = app.service("data");
+        let b = app.service("data");
+    "#,
+    );
+    let def = app.0.lock();
+    let count = def
+        .resources
+        .keys()
+        .filter(|id| id.kind == ResourceKind::Service && &*id.name == "data")
+        .count();
+    assert_eq!(count, 1);
+}
+
+// l[verify param.type]
+#[test]
+fn param_returns_placeholder() {
+    let app = run_test_script_app(r#"let x = app.param("foo");"#);
+    let def = app.0.lock();
+    assert!(def.params.contains_key("foo"));
+    assert_eq!(def.params["foo"], "<placeholder>");
+}
