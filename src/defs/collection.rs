d27b1b5ee95cc2aaa5b1ea441421f4752e4fd70a
Félix Saparelli <felix@bes.au>
2026-03-30 14:09:03 +1300

    an implementation was not actually an implementation

--- src/defs/collection.rs
+++ src/defs/collection.rs
@@ -4,22 +4,24 @@ use rhai::{CustomType, Dynamic, TypeBuilder};
 #[derive(Debug, Clone)]
 pub struct Collection;
 
-// l[impl collection.one]
-// l[impl collection.only]
-// l[impl collection.except]
-// l[impl collection.select]
-// l[impl collection.select.types]
-// l[impl collection.select.names]
-// l[impl collection.select.name-patterns]
 impl CustomType for Collection {
     fn build(mut builder: TypeBuilder<Self>) {
         builder
             .with_name("Collection")
-            .with_fn("one", |_this: &mut Self| -> Dynamic { Dynamic::UNIT })
-            .with_fn("only", |this: &mut Self, _other: Dynamic| this.clone())
-            .with_fn("except", |this: &mut Self, _other: Dynamic| this.clone())
-            .with_fn("select", |this: &mut Self, _criterion: rhai::Map| {
-                this.clone()
-            });
+            // l[impl collection.one]
+            .with_fn("one", |_this: &mut Self| -> Dynamic { todo!() })
+            // l[impl collection.only]
+            .with_fn("only", |_this: &mut Self, _other: Dynamic| -> Dynamic {
+                todo!()
+            })
+            // l[impl collection.except]
+            .with_fn("except", |_this: &mut Self, _other: Dynamic| -> Dynamic {
+                todo!()
+            })
+            // l[impl collection.select]
+            .with_fn(
+                "select",
+                |_this: &mut Self, _criterion: rhai::Map| -> Dynamic { todo!() },
+            );
     }
 }
